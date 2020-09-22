613257ed508b6b2769194f65f8fc46fc47df3716
Heather Peterson <hpeterson462@gmail.com>
2020-09-21 17:49:58 -0700

    post and get red endpoints passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,5 +8,22 @@ describe('createResponse', () => {
 
     expect(response.text)
       .toEqual('hi');
-  })
+  });
+
+  it('returns status code 200 to request body', async () => {
+    const response = await request(app)
+      .post('/echo')
+      .send({ body: 'plain text', status: '200 OK' })
+
+    expect(response.text)
+      .toEqual('plain text');
+  });
+
+  it('returns html with h1 and the word red', async () => {
+    const response = await request(app)
+      .get('/red')
+
+    expect(response.text)
+      .toEqual('<html><body><h1>red</h1></body></html>');
+  });
 });
