8940036ba30951b717f47fc02341260360de8aba
Heather Peterson <hpeterson462@gmail.com>
2020-09-21 17:27:29 -0700

    create response hi passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,11 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
+  it('returns hi', async () => {
+    const response = await request(app)
+      .get('/');
 
+    expect(response.text)
+      .toEqual('hi');
+  })
 });
